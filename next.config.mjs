c1326860456df004cbcc112971991845370bc091
abdolrhman-mo <abdelrhamnlearn@gmail.com>
2024-09-10 13:23:52 +0300

    added env var to image optimaization in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,15 @@
 
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-              protocol: 'http',
-              hostname: 'localhost',
-              port: '8000', // Add this if you're serving images from a specific port
-              pathname: '/media/products/**', // Adjust the path to match your image URLs
-            },
-        ],
-    }
-};
+      remotePatterns: [
+        {
+            protocol: new URL(process.env.NEXT_PUBLIC_API_URL).protocol.replace(':', ''), // Extracts protocol from env var
+            hostname: new URL(process.env.NEXT_PUBLIC_API_URL).hostname, // Extracts hostname from env var
+            port: new URL(process.env.NEXT_PUBLIC_API_URL).port || '', // Extracts port if available
+            pathname: '/media/products/**', // Adjust the path to match your image URLs
+        },
+    ],
+  }
+}
 
-export default nextConfig;
+export default nextConfig
